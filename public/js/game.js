e5ee85d626bb423d3c56315cf7ada169dad4cf70
codedivision <user@codedivision.my>
2015-06-16 16:56:06 +0800

    Draw board, set up functions to draw O and X

--- public/js/game.js
+++ public/js/game.js
@@ -1,18 +1,67 @@
 $(document).ready(function() {
-  var c = document.getElementById("board");
-  var ctx = c.getContext("2d");
-  ctx.fillStyle = "#dedede";
-  ctx.fillRect(0,0,300,300);
-  ctx.moveTo(0, 100);
-  ctx.lineTo(300, 100);
-  ctx.stroke();
-  ctx.moveTo(0, 200);
-  ctx.lineTo(300, 200);
-  ctx.stroke();
-  ctx.moveTo(100, 0);
-  ctx.lineTo(100, 300);
-  ctx.stroke();
-  ctx.moveTo(200, 0);
-  ctx.lineTo(200, 300);
-  ctx.stroke();
+
+  function drawBoard(){
+    var canvas = document.getElementById("board");
+    var context = canvas.getContext("2d");
+    // Fill big box with background color
+    context.fillStyle = "#dedede";
+    context.fillRect(0,0,300,300);
+
+    // Draw border for smaller internal boxes
+    context.beginPath();
+    context.moveTo(0, canvas.height/3);
+    context.lineTo(canvas.height, canvas.height/3);
+    context.stroke();
+    context.moveTo(0, canvas.height/3*2);
+    context.lineTo(canvas.height, canvas.height/3*2);
+    context.stroke();
+    context.moveTo(canvas.height/3, 0);
+    context.lineTo(canvas.height/3, canvas.height);
+    context.stroke();
+    context.moveTo(canvas.height/3*2, 0);
+    context.lineTo(canvas.height/3*2, canvas.height);
+    context.stroke();
+  };
+
+  function Box(){
+    var canvas = document.getElementById("board");
+    var context = canvas.getContext("2d");
+    this.drawO = function(coordX,coordY){
+      var centerX = (coordX * 100) + canvas.width/6
+      var centerY = (coordY * 100) + canvas.height/6
+      var radius = 30;
+      context.beginPath();
+      context.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+      context.stroke();
+    }
+
+    this.drawX= function(coordX, coordY){
+      var positionX = coordX * 100 + 20;
+      var positionY = coordY * 100 + 20;
+      context.moveTo(positionX, positionY);
+      context.lineTo(positionX + 60, positionY + 60);
+      context.stroke();
+      context.moveTo(positionX + 60, positionY);
+      context.lineTo(positionX, positionY + 60);
+      context.stroke();
+    };
+  };
+
+
+
+
+  $("#board").click(function(event){
+    event.preventDefault();
+    var x = Math.floor((event.clientX - 200)/100);
+    var y = Math.floor((event.clientY - 100)/100);
+    var coor = "X coords: " + x + ", Y coords: " + y;
+    document.getElementById("demo").innerHTML = coor;
+
+    var box = new Box();
+    box.drawX(x,y);
+
+  });
+
+  drawBoard();
+
 });
